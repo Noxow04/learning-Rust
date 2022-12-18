33a7c08c6de7951bac232dfad09c5095c3f425ac
Noxow <aguiral04@gmail.com>
2022-12-18 22:55:45 +0100

    first function (fibonacci)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    println!("{:?}", fibonacci(10))
+}
 
+fn fibonacci(n: usize) -> Vec<u64> {
+    let mut list = vec![0];
+    let mut previous_value: u64 = 0;
+    let mut value: u64 = 1;
+    for iter in 0..n {
+        value += previous_value;
+        previous_value = list[iter];
+        list.push(value);
+    }
+    list
 }
